b7b41bce3555d52deff6ada35761956c8bdaaa43
ahmedyar123 <ahmeda.abbasi99@gmail.com>
2020-09-30 03:45:43 +0500

    Add classes and onClick in InfoBox.js

--- src/components/InfoBox.js
+++ src/components/InfoBox.js
@@ -1,17 +1,24 @@
 import React from "react";
-
+import "./InfoBox.css";
 import { Card, CardContent, Typography } from "@material-ui/core";
 
-function InfoBox({ title, cases, total }) {
+function InfoBox({ title, cases, isRed, active, total, ...props }) {
   return (
-    <Card>
+    <Card
+      onClick={props.onClick}
+      className={`infoBox ${active && "infoBox--selected"} ${
+        isRed && "infoBox--red"
+      }`}
+    >
       <CardContent>
         {/* Title */}
         <Typography className="infoBox_title" color="textSecondary">
           {title}
         </Typography>
         {/* Number of cases today*/}
-        <h2 className="infoBox_cases">{cases}</h2>
+        <h2 className={`infoBox_cases ${!isRed && "infoBox_cases--green"}`}>
+          {cases}
+        </h2>
         {/* Total cases */}
         <Typography className="infoBox_total" color="textSecondary">
           {total} Total
